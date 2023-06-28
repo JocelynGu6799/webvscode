a2c9065d65d7a97fe65e0a583492411df9fc5595
JocelynGu6799 <2787912196@qq.com>
2023-06-28 09:37:18 +0800

    Changes

--- back/student_manage_system/src/main/java/org/example/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.example.pojo.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
--- back/student_manage_system/src/main/java/org/example/pojo/User.java
+++
@@ -1,9 +0,0 @@
-package org.example.pojo;
-
-import lombok.Data;
-
-@Data
-public class User {
-    private String username;
-    private String password;
-}
